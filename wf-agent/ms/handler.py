a0a38123e8f6058f692feabf8e5f8d041fd438c3
gabryk9 <48953262+gabryk9@users.noreply.github.com>
2022-07-12 12:28:47 +0200

    feat: intermediate function execution authentication using JWT

--- wf-agent/ms/handler.py
+++ wf-agent/ms/handler.py
@@ -1,9 +1,14 @@
 import json
+from lib2to3.pgen2 import token
 from requests import request
 from os import getenv, path
 from time import time
 from redis import Redis, lock
 from redis.commands.json.path import Path
+from typing import Union
+from jose import jwt
+from datetime import datetime, timedelta
+import constants
 
 
 this ={'path': path.dirname(path.realpath(__file__)), "host": getenv("THIS") }
@@ -29,7 +34,6 @@ def new_execution(r, wf, exec_id):
     }
     r.json().set(exec_id, Path.rootPath(), doc)
 
-
 def update_execution(r, wf, exec_id, state, output):
     doc = r.json().get(exec_id)
     doc["outputs"][state] = {
@@ -42,7 +46,6 @@ def update_execution(r, wf, exec_id, state, output):
     r.json().set(exec_id, Path.rootPath(), doc)
     return activable
 
-
 def find_activable(wf, exec):
     vars = {state: state in exec["outputs"].keys() for state in wf["states"].keys()}
     locals().update(vars)
@@ -52,33 +55,37 @@ def find_activable(wf, exec):
             activable.append(state)
     return activable
         
-
-def openfaas_invoker(conf, data, session):
+def openfaas_invoker(conf, data):
     # TODO: merge config in the State and in the function definition. Give priority to the State. 
     
     headers = conf["config"]
     headers["X-Callback-Url"] = f'{this["host"]}exec'
     print("Invoke", conf["endpoint"], headers)
-    resp = request("POST", url=conf["endpoint"], headers=headers, data=json.dumps(data))
-    print(resp.headers)
+    request("POST", url=conf["endpoint"], headers=headers, data=json.dumps(data))
 
-
-
-def trigger(wf, state, data, session):
+def trigger(wf, state, data):
     print(f"I am triggering {state}")
     fun_ref = wf["states"][state]["function"]["ref"]
     fun = wf["functions"][fun_ref]
     if fun["platform"] == "openfaas":
-        openfaas_invoker(fun, data, session)
+        openfaas_invoker(fun, data)
     else:
         raise ValueError("Platform not supported")
 
-def finalize(r, url, data):
+def finalize(url, data):
     exec_id = data['ctx']['execID']
     print(f"Finalizing {exec_id} to", url)
     request("POST", url=url, data=json.dumps(data["data"]))
     
-
+def create_exec_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, constants.SECRET_KEY, algorithm=constants.ALGORITHM)
+    return encoded_jwt
 
 def get_workflow(workflows, wid):
     if wid not in workflows:
@@ -96,9 +103,8 @@ def get_workflow(workflows, wid):
 def wf_trigger(req):
     print("Received body", req, type(req))
     wid = f'workflow.{req["ctx"]["workflowID"]}'
-    r = Redis(host=redis_host, password=redis_password, port=redis_port)
+    r = get_redis()
     wf = r.json().get(wid)
-    
     
     state = req["ctx"]["state"]
     exec_id = req["ctx"]["execID"]
@@ -108,6 +114,14 @@ def wf_trigger(req):
     req["ctx"]["execID"] = exec_id
     req["ctx"]["state"] = "__invoke__"
 
+    token_payload = req["ctx"].copy()
+    del token_payload["state"]
+    #print(f'to_encode: {to_encode}')
+    expires_delta = timedelta(minutes=constants.ACCESS_TOKEN_EXPIRE_MINUTES)
+    exec_token = create_exec_token(token_payload, expires_delta)
+    #print(exec_token)
+    req["token"] = exec_token
+    
     lockname = f'lock-{exec_id}'
     sem = lock.Lock(redis=r, name=lockname, timeout=60)
     sem.acquire()
@@ -127,12 +141,12 @@ def wf_trigger(req):
         session = None
         for act_state in activable:
             req["ctx"]["state"] = act_state
-            trigger(wf, act_state, req, session)
+            trigger(wf, act_state, req)
 
 def handle(req):
     print("Received body", req, type(req))
     wid = f'workflow.{req["ctx"]["workflowID"]}'
-    r = Redis(host=redis_host, password=redis_password, port=redis_port)
+    r = get_redis()
     wf = r.json().get(wid)
     
     state = req["ctx"]["state"]
@@ -152,7 +166,7 @@ def handle(req):
         session = None
         for act_state in activable:
             req["ctx"]["state"] = act_state
-            trigger(wf, act_state, req, session)
+            trigger(wf, act_state, req)
     if "end" in wf["states"][state] and wf["states"][state]["end"]:
-        finalize(r, wf["callbackUrl"], req)
+        finalize(wf["callbackUrl"], req)
 
--- wf-agent/ms/web.py
+++ wf-agent/ms/web.py
@@ -3,17 +3,13 @@ from fastapi import FastAPI, Response, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from handler import wf_trigger, handle
+from handler import wf_trigger, handle, get_redis
 from typing import Union, Any, Dict
 from pydantic import BaseModel
 import uvicorn
 import traceback
 import json
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+import constants
 
 fake_users_db = {
     "johndoe": {
@@ -25,15 +21,16 @@ fake_users_db = {
     }
 }
 
-
 class Token(BaseModel):
     access_token: str
     token_type: str
 
-
 class TokenData(BaseModel):
     username: Union[str, None] = None
 
+class WFTokenData(BaseModel):
+    wid: Union[str, None] = None
+    exec_id: Union[str, None] = None
 
 class User(BaseModel):
     username: str
@@ -42,7 +39,6 @@ class User(BaseModel):
 
 class UserInDB(User):
     hashed_password: str
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -88,7 +84,6 @@ def authenticate_user(fake_db, username: str, password: str):
         return False
     return user
 
-
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
     if expires_delta:
@@ -96,9 +91,8 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, constants.SECRET_KEY, algorithm=constants.ALGORITHM)
     return encoded_jwt
-
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -107,7 +101,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, constants.SECRET_KEY, algorithms=[constants.ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
@@ -119,6 +113,35 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return user
 
+def decode_exec_token(token: str):
+    try:
+        payload = jwt.decode(token, constants.SECRET_KEY, algorithms=[constants.ALGORITHM])
+        #print(f"decoded payload: {payload}")
+        wid: str = payload.get("workflowID")
+        exec_id: str = payload.get("execID")
+        #exp = payload.get("exp")
+        #if wid is None or exec_id is None or exp is None:
+        if wid is None or exec_id is None:
+            print("Not valid exec token: wid or exec_id None")
+            return None
+        else:
+            """ if(exp >= datetime.utcnow()):
+                print("Not valid exec token")
+                return None
+            else:
+                token_data = WFTokenData(wid=wid, exec_id = exec_id) """
+            token_data = WFTokenData(wid=wid, exec_id = exec_id)
+    except JWTError:
+        print("Not valid exec token: JWT ERROR")
+        return None
+    r = get_redis()
+    wf = r.json().get("workflow." + token_data.wid)
+    execution = r.json().get(token_data.exec_id)
+    if wf is None or execution is None:
+        print("Not valid exec token: not found WF or Exec")
+        return None
+    return token_data
+
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
@@ -128,7 +151,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=constants.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
@@ -166,12 +189,19 @@ def event(request: Union[Dict,Any], response: Response):
     elif type(request) != dict:
         raise ValueError("type in body not recognized")
     try:
-        handle(request)
-        response.status_code = 202
+        #print(f"request{request}")
+        token_data = decode_exec_token(request["token"])
+        #print(f"token-data: {token_data}")
+        if token_data is None:
+            response.status_code = 401
+        elif token_data.exec_id == request["ctx"]["execID"] and token_data.wid == request["ctx"]["workflowID"]:
+            handle(request)
+            response.status_code = 202
+        else:
+            response.status_code = 401
     except Exception as e:
         print(e)
         traceback.print_exc()
-        
         response.status_code = 501
     return
 
