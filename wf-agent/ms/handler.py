a85b8ed078c99b540de4c897e21472b1e6aee236
Gabriele Morabito <gabriele.morabito96@gmail.com>
2023-03-02 09:38:00 +0000

    feat: get secrets from vault

--- wf-agent/ms/handler.py
+++ wf-agent/ms/handler.py
@@ -8,13 +8,30 @@ from redis.commands.json.path import Path
 from typing import Union
 from jose import jwt
 from datetime import datetime, timedelta
-import constants
+import base64
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 this ={'path': path.dirname(path.realpath(__file__)), "host": getenv("THIS") }
 redis_host = getenv("REDIS_HOST") or "localhost"
 redis_password = getenv("REDIS_PASS") or ""
 redis_port = getenv("REDIS_PORT") or 6379
+
+openfaas_admin = "admin"
+openfaas_password = ""
+
+with open('/vault/secrets/openfaas', 'r') as f:
+    secret = json.load(f)
+    openfaas_admin = secret["data"]["admin"]
+    openfaas_password = secret["data"]["password"]
+
+secret_key = ""
+with open('/vault/secrets/jwt', 'r') as f:
+    secret = json.load(f)
+    secret_key = secret["data"]["key"]
+
+algorithm = getenv("JWT_ALGORITHM")
+expire_minutes = int(getenv("JWT_EXPIRE_MINUTES"))
 
 def get_redis():
     r = Redis(host=redis_host, password=redis_password, port=redis_port)
@@ -60,13 +77,67 @@ def openfaas_invoker(conf, data):
     
     headers = conf["config"]
     headers["X-Callback-Url"] = f'{this["host"]}exec'
+    #headers["X-Callback-Url"] = 'http://172.16.1.176:31114/exec'
     print("Invoke", conf["endpoint"], headers)
+    print("Data sent:")
+    print(data)
     request("POST", url=conf["endpoint"], headers=headers, data=json.dumps(data))
+
+def encrypt(data,key,iv):
+    print("to be encrypted:")
+    print(data)
+
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+    enc = cipher.encryptor()
+
+    data_string = json.dumps(data)
+    data_bytes = bytes(data_string,'utf-8')
+
+    data_encrypted = str(base64.b64encode(enc.update(data_bytes) + enc.finalize()))
+
+    return data_encrypted
+
+def decrypt(data,key,iv):
+    print("to be decrypted:")
+    print(data)
+
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+    dec = cipher.decryptor()
+
+    b64decoded_cdata = base64.b64decode(eval(data))
+    
+    data_decrypted = eval((dec.update(b64decoded_cdata) + dec.finalize()).decode("utf-8"))
+
+    if (type(data_decrypted) == str):
+        data = json.dumps(data_decrypted)
+
+    return data_decrypted
 
 def trigger(wf, state, data):
     print(f"I am triggering {state}")
     fun_ref = wf["states"][state]["function"]["ref"]
     fun = wf["functions"][fun_ref]
+
+     ######  QUA CIFRARE DATI CON CHIAVE STATO NUOVO ######
+    crypted = False 
+    if "crypted" in wf["functions"][fun_ref]:
+        crypted = wf["functions"][fun_ref]["crypted"]
+
+    if crypted and type(data["data"]) != str:
+        endpoint = fun["endpoint"]
+        role = endpoint.split(".")[-1]
+        if role == "openfaas-fn":
+            role = "world"
+        key = ""
+        iv = ""
+        with open('/vault/secrets/' + role, 'r') as f:
+            secret = json.load(f)
+            key = bytes(secret["data"]["key"], 'utf-8')
+            iv = bytes(secret["data"]["iv"], 'utf-8')
+        data["data"] = encrypt(data["data"], key, iv)
+   
+   
+
     if fun["platform"] == "openfaas":
         openfaas_invoker(fun, data)
     else:
@@ -84,7 +155,7 @@ def create_exec_token(data: dict, expires_delta: Union[timedelta, None] = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, constants.SECRET_KEY, algorithm=constants.ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm)
     return encoded_jwt
 
 def get_workflow(workflows, wid):
@@ -100,11 +171,33 @@ def get_workflow(workflows, wid):
         print(f"Workflow {wid} in cache")
     return workflows[wid]
 
-def wf_trigger(req):
+def wf_trigger(req, username):
     print("Received body", req, type(req))
     wid = f'workflow.{req["ctx"]["workflowID"]}'
     r = get_redis()
-    wf = r.json().get(wid)
+    
+    wf = r.json().get(f"{wid}")
+    user = r.json().get(username)
+
+    allowed = user["groups"]
+    allowed.append(username)
+    allowed.append("openfaas-fn")
+
+    functions = wf["functions"]
+
+    scopes = set()
+
+    for key in functions.keys():
+        endpoint = functions[key]["endpoint"]
+        scope = endpoint.split(".")[-1]
+        scopes.add(scope)
+    
+    """ print(allowed)
+    print(scopes) """
+
+    for scope in scopes:
+        if scope not in allowed:
+            return False
     
     state = req["ctx"]["state"]
     exec_id = req["ctx"]["execID"]
@@ -117,10 +210,11 @@ def wf_trigger(req):
     token_payload = req["ctx"].copy()
     del token_payload["state"]
     #print(f'to_encode: {to_encode}')
-    expires_delta = timedelta(minutes=constants.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expires_delta = timedelta(minutes=expire_minutes)
     exec_token = create_exec_token(token_payload, expires_delta)
     #print(exec_token)
-    req["token"] = exec_token
+    req["exec_token"] = exec_token
+    #req["token"] = token
     
     lockname = f'lock-{exec_id}'
     sem = lock.Lock(redis=r, name=lockname, timeout=60)
@@ -136,12 +230,13 @@ def wf_trigger(req):
     activable = update_execution(r, wf, exec_id, state, output)
     print("Releasing semaphore")
     sem.release()
-    
+    ##### QUA NON SI DECIFRA #######
     if len(activable) > 0:
         session = None
         for act_state in activable:
             req["ctx"]["state"] = act_state
             trigger(wf, act_state, req)
+    return True
 
 def handle(req):
     print("Received body", req, type(req))
@@ -162,6 +257,27 @@ def handle(req):
     activable = update_execution(r, wf, exec_id, state, output)
     print("Releasing semaphore")
     sem.release()
+
+    ##### QUA DECIFRARE CON CHIAVE STATO RICEVUTO #######
+
+    crypted = False 
+    fun_ref = wf["states"][state]["function"]["ref"]
+    if "crypted" in wf["functions"][fun_ref]:
+        crypted = wf["functions"][fun_ref]["crypted"]
+    if crypted:
+        endpoint = wf["functions"][fun_ref]["endpoint"]
+        role = endpoint.split(".")[-1]
+        if role == "openfaas-fn":
+            role = "world"
+        key = ""
+        iv = ""
+        with open('/vault/secrets/' + role, 'r') as f:
+            secret = json.load(f)
+            key = bytes(secret["data"]["key"], 'utf-8')
+            iv = bytes(secret["data"]["iv"], 'utf-8')
+        req["data"] = decrypt(req["data"], key, iv)
+
+
     if len(activable) > 0:
         session = None
         for act_state in activable:
@@ -170,3 +286,66 @@ def handle(req):
     if "end" in wf["states"][state] and wf["states"][state]["end"]:
         finalize(wf["callbackUrl"], req)
 
+def el_deploy(req, username, update):
+    print("Received body", req, type(req))
+    r = get_redis()
+    user = r.json().get(username)
+
+    allowed = [username, "openfaas-fn"]
+
+    if req["group"] == True:
+        allowed = user["groups"]
+    
+    if req["namespace"] not in allowed:
+        return 401
+
+    constraints = []
+    role = ""
+
+    if req["namespace"] != "openfaas-fn":
+        constraints = [req["namespace"] + "=true"]
+        role = req["namespace"]
+    else:
+        role= "world"
+
+    annotations = dict()
+
+    if req["cryptography"]:
+        annotations = annotate_function(role)
+
+    headers = {}
+    headers["Content-Type"] = "application/json"
+    headers["Authorization"] = "Basic " + base64.b64encode((openfaas_admin + ":" + openfaas_password).encode("ascii")).decode("ascii")
+    
+    payload = {
+        "service": req["service"],
+        "image": req["image"],
+        "constraints": constraints,
+        "labels": req["labels"],
+        "annotations": annotations,
+        "secrets": req["secrets"],
+        "namespace": req["namespace"]
+    }
+    
+    resp = "Empty response"
+
+    if(update):
+        resp = request("PUT", url="http://gateway.openfaas.svc.cluster.local:8080/system/functions", headers=headers, data=json.dumps(payload))
+    else:
+        resp = request("POST", url="http://gateway.openfaas.svc.cluster.local:8080/system/functions", headers=headers, data=json.dumps(payload))
+    print(resp)
+    return resp.status_code
+    if (resp.status_code == 202):
+        return True
+    else:
+        return False
+
+def annotate_function(role):
+    annotations = dict()
+    annotations["vault.hashicorp.com/agent-inject"] = "true"
+    annotations["vault.hashicorp.com/secret-volume-path"] = "/var/openfaas/secrets"
+    annotations["vault.hashicorp.com/agent-inject-secret-secret_key"] = "openfaas/" + role + "-secret"
+    annotations["vault.hashicorp.com/agent-inject-status"] = "update"
+    annotations["vault.hashicorp.com/role"] = role
+    return annotations
+    
